fbc79eea99fbeb37c16189ee3484437d82048ec1
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-10-29 16:03:08 +0700

    fix

--- bot/__init__.py
+++ bot/__init__.py
@@ -135,7 +135,7 @@ if len(TELEGRAM_HASH) == 0:
     
 TIMEZONE = environ.get('TIMEZONE', '')
 if len(TIMEZONE) == 0:
-    TIMEZONE = 'Asia/Kolkata'
+    TIMEZONE = 'Asia/Makassar'
     
 def changetz(*args):
     return datetime.now(timezone(TIMEZONE)).timetuple()
@@ -507,7 +507,7 @@ if len(TITLE_NAME) == 0:
     
 COVER_IMAGE = environ.get('COVER_IMAGE', '')
 if len(COVER_IMAGE) == 0:
-    COVER_IMAGE = 'https://graph.org/file/60f9f8bcb97d27f76f5c0.jpg'
+    COVER_IMAGE = 'https://te.legra.ph/file/2dd694fa7318e79df3423.jpg'
 
 GD_INFO = environ.get('GD_INFO', '')
 if len(GD_INFO) == 0:
