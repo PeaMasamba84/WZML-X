f9068de2501815c1315ef6f5e1542b33d629a6d9
PeaMasamba84 <104630285+PeaMasamba84@users.noreply.github.com>
2023-10-29 12:30:51 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -243,7 +243,9 @@ def get_readable_message():
         msg += f"\n<b>User: </b>{download.message.from_user.mention(style='html')} ID: </b><code>{download.message.from_user.id}</code>"        
         if (download.eng()).startswith("qBit"):
             msg += BotTheme('BTSEL', Btsel=f"/{BotCommands.BtSelectCommand}_{download.gid()}")
-         msg += f"\n <b>Stop:</b> <code>/{BotCommands.CancelMirror}_{download.gid()}</code>\n\n"
+         msg += f"\n<b>Stop: </b><code>/{BotCommands.CancelMirror} {download.gid()}</code>\n\n"
+        
+    if len(msg) == 0:
 
     if len(msg) == 0:
         return None, None
